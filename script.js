58afe6b99b820b8ac1b4ea12a9ac954b145f2177
Kevin Chaires <37939130+KevinChaires@users.noreply.github.com>
2023-02-19 02:13:21 -0600

    Add game() function with 5 rounds

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function getComputerChoice(){
     else{
         choice = 'scissors';
     }
-    console.log(choice);
     return choice;
 }
 
@@ -19,36 +18,40 @@ function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
         result = `A tie! Both you and the computer chose ${playerSelection}!`;
     }
-    else if(playerSelection === 'rock'){
-        if(computerSelection === 'paper'){
-            result = 'You lose. Paper beats Rock.';
-        }
-        else if(computerSelection === 'scissors'){
-            result = 'You win! Rock beats Scissors.'
-        }
+    else if(playerSelection === 'rock' && computerSelection === 'paper'){
+        result = 'You lose. Paper beats Rock.';
     }
-    else if (playerSelection === 'paper'){
-        if(computerSelection === 'rock'){
-            result = 'You win! Paper beats Rock.';
-        }
-        else if (computerSelection === 'scissors'){
-            result = 'You lose. Scissors beat Paper.';
-        }
+    else if(playerSelection === 'rock' && computerSelection === 'scissors'){
+        result = 'You win! Rock beats Scissors.'
     }
-    else if (playerSelection === 'scissors'){
-        if(computerSelection === 'rock'){
-            result = 'You lose. Rock beats Scissors.';
-        }
-        else if(computerSelection === 'paper'){
-            result = 'You win! Scissors beat Paper.'
-        }
+    else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        result = 'You win! Paper beats Rock.';
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+        result = 'You lose. Scissors beat Paper.';
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+        result = 'You lose. Rock beats Scissors.';
+    }
+    else if(playerSelection === 'scissors' && computerSelection === 'paper'){
+        result = 'You win! Scissors beat Paper.'
     }
     else{
-       result = 'Error.';
+       result = 'Error. Either you chose an invalid choice, or I made a mistake!';
     }
+
     return(result);
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+    console.log('This is a game of Rock, Papers, Scissors against the computer');
+    for (let i = 0; i < 5; i++){
+        var round = i+1;
+        console.log('Round ' + round);
+        var playerSelection = prompt('Choose Rock, Paper or Scissors').toLowerCase();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
