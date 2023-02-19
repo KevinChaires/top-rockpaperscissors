91560aca449cebf11999c09ce4458a5405743fd7
Kevin Chaires <37939130+KevinChaires@users.noreply.github.com>
2023-02-19 01:58:18 -0600

    Add playRound() function

--- script.js
+++ script.js
@@ -2,15 +2,53 @@ function getComputerChoice(){
     let rand = Math.floor(Math.random() *  (3) + 1);
     let choice;
     if(rand === 1){
-        choice = 'Rock';
+        choice = 'rock';
     }
     else if(rand === 2){
-        choice = 'Paper';
+        choice = 'paper';
     }
     else{
-        choice = 'Scissors';
+        choice = 'scissors';
     }
+    console.log(choice);
     return choice;
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection){
+    let result;
+    if (playerSelection === computerSelection){
+        result = `A tie! Both you and the computer chose ${playerSelection}!`;
+    }
+    else if(playerSelection === 'rock'){
+        if(computerSelection === 'paper'){
+            result = 'You lose. Paper beats Rock.';
+        }
+        else if(computerSelection === 'scissors'){
+            result = 'You win! Rock beats Scissors.'
+        }
+    }
+    else if (playerSelection === 'paper'){
+        if(computerSelection === 'rock'){
+            result = 'You win! Paper beats Rock.';
+        }
+        else if (computerSelection === 'scissors'){
+            result = 'You lose. Scissors beat Paper.';
+        }
+    }
+    else if (playerSelection === 'scissors'){
+        if(computerSelection === 'rock'){
+            result = 'You lose. Rock beats Scissors.';
+        }
+        else if(computerSelection === 'paper'){
+            result = 'You win! Scissors beat Paper.'
+        }
+    }
+    else{
+       result = 'Error.';
+    }
+    return(result);
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
