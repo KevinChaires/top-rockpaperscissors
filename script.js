1815ae6708bdba2cbea673924f1fa390c538c1bc
Kevin Chaires <37939130+KevinChaires@users.noreply.github.com>
2023-02-23 18:14:13 -0600

    Add UI to Rock, Papers, Scissors

--- script.js
+++ script.js
@@ -18,36 +18,64 @@ function playRound(playerSelection, computerSelection){
     let result;
     if (playerSelection === computerSelection){
         result = `A tie! Both you and the computer chose ${playerSelection}!`;
+        userColor(5);
+        PCColor(5);
     }
     //Player chose Rock
     else if(playerSelection === 'rock' && computerSelection === 'paper'){
         result = 'You lose. Paper beats Rock.';
         computerCount = computerCount + 1;
+        userChoice.textContent = '🪨';
+        PCChoice.textContent = "📃";
+        userColor(3);
+        PCColor(1);
     }
     else if(playerSelection === 'rock' && computerSelection === 'scissors'){
         result = 'You win! Rock beats Scissors.'
         playerCount = playerCount + 1;
+        userColor(1);
+        PCColor(3);
+        userChoice.textContent = '🪨';
+        PCChoice.textContent = '✂️';
     }
     //Player chose Paper
     else if (playerSelection === 'paper' && computerSelection === 'rock'){
         result = 'You win! Paper beats Rock.';
         playerCount = playerCount + 1;
+        userChoice.textContent = "📃";
+        PCChoice.textContent = '🪨';
+        userColor(1);
+        PCColor(3);
     }
     else if (playerSelection === 'paper' && computerSelection === 'scissors'){
         result = 'You lose. Scissors beat Paper.';
         computerCount = computerCount + 1;
+        userChoice.textContent = "📃";
+        PCChoice.textContent = '✂️';
+        userColor(3);
+        PCColor(1);
     }
     //Player chose Scissors
     else if (playerSelection === 'scissors' && computerSelection === 'rock'){
         result = 'You lose. Rock beats Scissors.';
         computerCount = computerCount + 1;
+        userColor(3);
+        PCColor(1);
+        userChoice.textContent = '✂️';
+        PCChoice.textContent = '🪨';
     }
     else if(playerSelection === 'scissors' && computerSelection === 'paper'){
         result = 'You win! Scissors beat Paper.'
         playerCount = playerCount + 1;
+        userChoice.textContent = '✂️';
+        PCChoice.textContent = "📃";
+        userColor(1);
+        PCColor(3);
     }
     else{
        result = 'Error. Either you chose an invalid choice, or I made a mistake!';
+       //userColor(5);
+       //PCColor(5);
     }
     return(result);
 }
@@ -56,49 +84,29 @@ function gameWinner(){
     let result;
     let playerNumber = parseInt(playerCount);
     let computerNumber = parseInt(computerCount);
+    let ender = 0;
     if(playerNumber === computerNumber){
+        ender = userColor(1);
+        ender = PCColor(1);
         result = `A tie! ${playerNumber} vs ${computerNumber}`
     }
     else if(playerNumber > computerCount){
+        ender = userColor(2);
+        ender = PCColor(4);
         result = `You won! ${playerNumber} vs ${computerNumber}`
     }
     else if(playerNumber < computerCount){
+        ender = userColor(4);
+        ender = PCColor(2);
         result = `You lost! ${playerNumber} vs ${computerNumber}`
     }
-    return result;
+    
+        return result;
 }
+
 
 let playerCount = 0;
 let computerCount = 0;  
-
-/*
-function game(){
-    console.log('This is a game of Rock, Papers, Scissors against the computer');
-  
-    for (let i = 0; i < 5; i++){
-        var round = i+1;
-        console.log('Round ' + round);
-        currentRound.textContent= 'Round ' + round;
-        
-        var playerSelection = prompt('Choose Rock, Paper or Scissors').toLowerCase();
-        
-        //playerSelection = playerSelected;
-        
-        const computerSelection = getComputerChoice();        
-        
-        roundResult.textContent = playRound(playerSelection, computerSelection, playerCount, computerCount);
-        
-        console.log(playRound(playerSelection, computerSelection, playerCount, computerCount));
-        
-        userScore.textContent = `USER: ${playerCount}`;
-        
-        PCScore.textContent = `PC: ${computerCount}`;
-    }
-    gameResult.textContent = gameWinner();
-    console.log(gameWinner());
-}
-*/
-
 let round = 0;
 
 function game(playerSelected){
@@ -170,23 +178,74 @@ notifications.appendChild(currentRound);
 //Notifier
 const notifier = document.querySelector('#notifier');
 
-//Round Result
-//let roundResult = document.createElement('p');
-//roundResult.textContent = "";
-//notifications.appendChild(roundResult);
-
-//Game Result
-//let gameResult = document.createElement('p');
-//gameResult.textContent = "";
-//notifications.appendChild(gameResult);
-
 //USER AND PC SCORE
 let userScore = document.querySelector('#userScore');
 userScore.textContent = 'USER: 0';
 
 let PCScore = document.querySelector('#PCScore');
-PCScore.textContent = 'USER: 0';
+PCScore.textContent = 'PC: 0';
 
+//USER AND PC CHOICE
+let userChoice = document.querySelector("#userChoice");
+userChoice.textContent = '___';
+let PCChoice = document.querySelector("#PCChoice");
+PCChoice.textContent = '___';
 
+//IMAGEN USUARIO
+let userPic = document.querySelector(".user");
+let userImg = document.querySelector(".userIMG")
 
-//Round, Result, RoundWinner, GameWinner
+function userColor (number){
+    switch(number){
+        case 1: //ROUND WON - LIGHT GREEN
+            userPic.style.backgroundColor = "#7CFA7F";
+            userImg.src = "./media/user0.png";
+            break;
+        case 2: //GAME WON - STRONG GREEN
+            userPic.style.backgroundColor = "#54BD37";
+            userImg.src = "./media/user2.png";
+            break;
+        case 3: //LOST ROUND - LIGHT RED
+            userPic.style.backgroundColor = "#F5625D";
+            userImg.src = "./media/user3.png";
+            break;
+        case 4: //LOST GAME - STRONG RED
+            userPic.style.backgroundColor = "#F5160F";
+            userImg.src = "./media/user4.png";
+            break;
+        default:
+            userPic.style.backgroundColor = "#7CFA7F";
+            userPic.style.backgroundColor = "#00C8FA";
+            break;
+    }
+    return number;
+}
+
+//IMAGEN PC
+let PCPic = document.querySelector('.PC');
+let PCImg = document.querySelector('.PCIMG');
+function PCColor (number){
+    switch(number){
+        case 1: //ROUND WON - LIGHT GREEN
+            PCPic.style.backgroundColor = "#7CFA7F";
+            PCImg.src = "./media/PC1.png";
+            break;
+        case 2: //GAME WON - STRONG GREEN
+            PCPic.style.backgroundColor = "#54BD37";
+            PCImg.src = "./media/PC2.png";
+            break;
+        case 3: //LOST ROUND - LIGHT RED
+            PCPic.style.backgroundColor = "#F5625D";
+            PCImg.src = "./media/PC3.png";
+            break;
+        case 4: //LOST GAME - STRONG RED
+            PCPic.style.backgroundColor = "#F5160F";
+            PCImg.src = "./media/PC4.png";
+            break;
+        default:
+            PCImg.src = "./media/PC1.png";
+            PCPic.style.backgroundColor = "#00C8FA";
+            break;
+    }
+    return number;
+}
